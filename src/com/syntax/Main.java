26b6c239822511238d805a2bec1a60c6074656d3
malfadhli <moefna93@gmail.com>
2022-01-25 20:55:39 -0500

    3rd Commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,5 +3,6 @@ package com.syntax;
 public class Main {
     public static void main(String[] args) {
         System.out.println("I'm batch 11");
+        System.out.println("I'm batch 12");
     }
 }
